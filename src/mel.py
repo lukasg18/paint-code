cd500a0d0244a2631ccd4428a449845b605e6c14
lukasg18 <lukas.gomes@hotmail.com>
2019-06-07 11:28:46 -0300

    b

--- src/mel.py
+++ src/mel.py
@@ -2,12 +2,12 @@ from lark import Lark
 
 class MEL():
     def __init__(self):
-        self._grammar = Lark(open('grammar/grammar.lark'), start='expr')
+        self._grammar = Lark(open('grammar/grammar.lark'))
     
     def parser(self, expression):
         grammar_parse = self._grammar
         try:
             parse_tree = grammar_parse.parse(expression)
             print("expressao aceita: ", parse_tree) 
-        except:
-            print("expressao invalida")
+        except Exception as e:
+            print(e)
