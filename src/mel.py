016079c9ff92dca0bc3979b6fe5d18bb27d30bf0
lukasg18 <lukas.gomes@hotmail.com>
2019-06-10 13:07:51 -0300

    teste na gramatica

--- src/mel.py
+++ src/mel.py
@@ -1,4 +1,5 @@
-from lark import Lark
+from lark import Lark, Tree
+import turtle
 
 class MEL():
     def __init__(self):
@@ -8,6 +9,79 @@ class MEL():
         grammar_parse = self._grammar
         try:
             parse_tree = grammar_parse.parse(expression)
+            for inst in parse_tree.children:
+                self.run_instruction(inst)
             print("expressao aceita: ", parse_tree) 
         except Exception as e:
             print(e)
+
+    def run_instruction(self, t):
+        if t.data == 'action':
+            for inst in t.children:
+                self.run_action(inst)
+
+        elif t.data == 'code_block':
+            for inst in t.children:
+                self.run_code_block(inst)
+        
+        elif t.data == 'loop':
+            for inst in t.children:
+                self.run_loop(inst)
+
+        elif t.data == 'clear':
+            for inst in t.children:
+                self.run_clear(inst)
+
+        elif t.data == 'function':
+            for inst in t.children:
+                self.run_function(inst)
+
+        elif t.data == 'assign':
+            for inst in t.children:
+                self.run_assign(inst)
+    
+    def run_action(self, t):
+        if t.data == 'movement':
+            print("func movement")
+        
+        elif t.data == 'custom_color':
+            for inst in t.children:
+                self.run_custom_color(inst)
+        
+        elif t.data == 'custom_background':
+            print("func custom_background")
+        
+    def run_code_block(self, t):
+        print("func code_block")
+        self.run_instruction(t.children[0])
+    
+    def run_loop(self, t):
+        print("func run_loop")
+        for i in range(len(t.children)):
+            ident_loop, count, block = t.children[i].children
+            for i in range(int(count)):
+                self.run_instruction(block)
+
+    def run_clear(self, t):
+        print("func clear")
+    
+    def run_function(self, t):
+        print("func function")
+        self.run_code_block(t)
+
+    def run_assign(self, t):
+        if t.data == 'custom_color':
+            print("func custom_color")
+        
+        elif t.data == 'lambda':
+            print("func lambda")
+
+        else:
+            print("number")
+    
+    def run_custom_color(self, t):
+        for inst in t.children:
+            if isinstance(inst, Tree) and inst.data == 'rgb':
+                print("func rgb")
+            else:
+                print("color")
