8e27c993086ce4eab94de4661b496fef4688264e
lukasg18 <lukas.gomes@hotmail.com>
2019-06-19 15:23:40 -0300

    e

--- src/mel.py
+++ src/mel.py
@@ -1,9 +1,38 @@
 from lark import Lark, Tree
 import turtle
 
+variables = {}
+
 class MEL():
     def __init__(self):
-        self._grammar = Lark(open('grammar/grammar.lark'))
+        self._grammar = Lark(open('grammar/new-grammar.lark'))
+    
+    def _isValidVariable(self, key):
+        if (not key in variables.keys()):
+            raise Exception('Variable "'+key+'" not defined')
+    
+    def _getRGB(self, children):
+        color = {'r': '', 'g': '', 'b': ''}
+        key = 'r'
+        pos = 0
+        keys = ['r', 'g', 'b']
+
+        for item in children:
+            if (isinstance(item, Tree) and item.data == 'variable'):
+                keyVar = item.children[0].value
+                self._isValidVariable(keyVar)
+                color[key] = variables[keyVar]
+                if pos < 2:
+                    pos += 1
+                key = keys[pos]
+                
+            else:
+                color[key] += str(item)
+                if (len(color[key]) == 3):
+                    if pos < 2:
+                        pos += 1
+                    key = keys[pos]
+        return (int(color['r']), int(color['g']), int(color['b']))
     
     def parser(self, expression):
         grammar_parse = self._grammar
@@ -12,6 +41,7 @@ class MEL():
             for inst in parse_tree.children:
                 self.run_instruction(inst)
             print("expressao aceita: ", parse_tree) 
+            print("variables: ", variables)
         except Exception as e:
             print(e)
 
@@ -20,29 +50,18 @@ class MEL():
             for inst in t.children:
                 self.run_action(inst)
 
-        elif t.data == 'code_block':
+        elif t.data == 'assign':
             for inst in t.children:
-                self.run_code_block(inst)
+                self.run_assign(inst)
         
         elif t.data == 'loop':
             for inst in t.children:
                 self.run_loop(inst)
-
-        elif t.data == 'clear':
-            for inst in t.children:
-                self.run_clear(inst)
-
-        elif t.data == 'function':
-            for inst in t.children:
-                self.run_function(inst)
-
-        elif t.data == 'assign':
-            for inst in t.children:
-                self.run_assign(inst)
     
     def run_action(self, t):
         if t.data == 'movement':
-            print("func movement")
+            for inst in t.children:
+                self.run_movement(inst)
         
         elif t.data == 'custom_color':
             for inst in t.children:
@@ -51,53 +70,80 @@ class MEL():
         elif t.data == 'custom_background':
             for inst in t.children:
                 self.run_custom_background(inst)
-        
-    def run_code_block(self, t):
-        print("func code_block")
-        for inst in t.children:
-            self.run_instruction(inst)
-    
-    def run_loop(self, t):
-        print("func run_loop")
-        count = 0
-        for i in range(len(t.children)):
-            child = t.children[i]
-            if (isinstance(child, Tree)):
-                child2 = child.children[0]
-                for k in range(count):
-                    self.run_code_block(child2)
-            else:
-                count = int(child)
-                print("NUMBER")
 
-    def run_clear(self, t):
-        print("func clear")
-    
-    def run_function(self, t):
-        print("func function")
-        print(t)
-        self.run_code_block(t)
+        elif t.data == 'clear':
+            turtle.clear()
+
+        elif t.data == 'reset':
+            turtle.clear()
+            turtle.bgcolor('white')
+            turtle.color('black')
+
+        elif t.data == 'fill':
+            for inst in t.children:
+                self.run_fill(inst)
 
     def run_assign(self, t):
-        if t.data == 'custom_color':
-            print("func custom_color")
-        
-        elif t.data == 'lambda':
-            print("func lambda")
-
-        else:
-            print("number")
+        name = t.children[0].value
+        value = t.children[1].value
+        variables[name] = int(value)
     
+    def run_fill(self, t):
+        for child in t.children:
+            if(child.type == 'BEGINFILL'):
+                turtle.begin_fill()
+            elif(child.type == 'ENDFILL'):
+                turtle.end_fill()
+
+    def run_loop(self, t):
+        number = 0
+        for child in t.children:
+            if (isinstance(child, Tree) and child.data == "variable"):
+                varKey = child.children[0].value
+                self._isValidVariable(varKey)
+                number = variables[varKey]
+            elif (isinstance(child, Tree) and child.data == "code_block"):
+                for i in range(number):
+                    self.run_code_block(child)
+            elif(child.type == 'NUMBER'):
+                number = int(child.value)
+
+    def run_code_block(self, t):
+        print(t)
+        for child in t.children:
+            self.run_action(child)
+
+    def run_movement(self, t):
+        aux = { 'f': turtle.fd,
+                    'b': turtle.bk,
+                    'l': turtle.lt,
+                    'r': turtle.rt, }
+        for child in t.children:
+            if (isinstance(child, Tree) and child.data == "variable"):
+                varKey = child.children[0].value
+                self._isValidVariable(varKey)
+                aux[direction](variables[varKey])
+            elif(child.type == 'DIRECTION'):
+                direction = child
+            else:
+                aux[direction](int(child))
+
     def run_custom_color(self, t):
+        # color rgb 000 000 000 
         for inst in t.children:
             if isinstance(inst, Tree) and inst.data == 'rgb':
-                print("func rgb")
+                turtle.colormode(255)
+                r,g,b = self._getRGB(inst.children)
+                turtle.pencolor((r, g, b))
             else:
-                print("color")
+                turtle.color(inst)
     
     def run_custom_background(self, t):
         for inst in t.children:
             if isinstance(inst, Tree) and inst.data == 'rgb':
-                print("func rgb")
+                r,g,b = self._getRGB(inst.children)
+                turtle.bgcolor(r,g,b)
             else:
-                print("color")
+                turtle.bgcolor(inst)
+
+     
