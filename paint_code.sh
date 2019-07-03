3a5ed81953b86668a4f0e81269fbc61dbb3d0fa0
lukasg18 <lukas.gomes@hotmail.com>
2019-07-03 15:16:02 -0300

    finalizado

--- paint_code.sh
+++ paint_code.sh
@@ -1,7 +1,10 @@
+echo "instalando ferramentas necessarias..."
 sudo apt-get -yqq install python3-pip python3-tk
+echo "instalando virtualenv..."
 sudo pip3 install virtualenv
 virtualenv venv --python=python3
 source venv/bin/activate
+echo "instalando depedencias do PaintCode..."
 pip install lark-parser argparse
 cd src/
 python main.py --file testes/example-frac2.pc
