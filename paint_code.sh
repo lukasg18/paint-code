aa3b3002f618619c2582797f07213a393937dc7b
Marcos A C de Paula <cardepaula@gmail.com>
2019-10-16 08:43:44 -0300

    Change to pipenv

--- paint_code.sh
+++ paint_code.sh
@@ -1,11 +1,14 @@
-echo "instalando ferramentas necessarias..."
+#!/bin/bash
+echo "\n_________instalando ferramentas necessarias..._________\n"
 sudo apt-get -yqq install python3-pip python3-tk
-echo "instalando virtualenv..."
-sudo pip3 install virtualenv
-virtualenv venv --python=python3
-source venv/bin/activate
-echo "instalando depedencias do PaintCode..."
-pip install lark-parser argparse
-cd src/
-python main.py --file testes/example-frac2.pc
+echo "\n_________instalando pipenv..."
+if sudo pip install pipenv
+then
+    echo "\n_________instalando depedencias do PaintCode..._________\n"
+    pipenv install
+    pipenv shell
+else
+    echo "\n_________Erro ao instalar pipenv_________\n"
+    exit 1
+fi
 
